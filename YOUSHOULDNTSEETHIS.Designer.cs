88c2f2de8f5f18411cbf71a6bf87fc657d323df6
marco <marcofernandioo@gmail.com>
2022-05-17 14:48:07 +0800

    added a new page

---
+++ YOUSHOULDNTSEETHIS.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace _1stdebug
+{
+    partial class YOUSHOULDNTSEETHIS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // YOUSHOULDNTSEETHIS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1929, 816);
+            this.Name = "YOUSHOULDNTSEETHIS";
+            this.Text = "YOUSHOULDNTSEETHIS";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
